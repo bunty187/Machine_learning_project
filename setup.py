e284e68080ee912051a4395491d027c832613a52
Bunty Kumar <92105094+bunty187@users.noreply.github.com>
2024-08-24 02:12:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,9 @@
-# from setuptools import find_packages ,setup 
-# from typing import List
-
-
-
-# def get_requirements()->List[str]:
-
-#     reuirements_list : List[str] =[]
-
-#     return reuirements_list
-
-
-
-
-
-
-# setup (
-#     name = 'sensor',
-#     version="0.0.1",
-#     author="prince",
-#     author_email="princekatiyar986@gmail.com",
-#     packages = find_packages(),
-#     install_requires = get_requirements() , #["pymongo"]
-
-# )
 
 from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
     """
-
-
     This function will return list of requirements
     """
     requirement_list:List[str] = []
@@ -43,8 +16,8 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.1",
-    author="ineuron",
-    author_email="princekatiyar986@gmail.com",
+    author="buntykumar",
+    author_email="buntyk3623@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
